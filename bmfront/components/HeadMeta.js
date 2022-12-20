35d35818cb26d738b6a562c67aae77a1b649c63a
Eomhyunjun <zzp1318@gmail.com>
2022-12-20 20:09:20 +0900

    SEO: 네이버 소유확인

---
+++ bmfront/components/HeadMeta.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+function HeadMeta(props) {
+    return (
+        <Head>
+            <meta name="naver-site-verification" content="e5b01ccf23389c9c3316a778fe94b575e0a8f699" />
+        </Head>
+    );
+}
+
+export default HeadMeta;
--- bmfront/pages/_app.js
+++ bmfront/pages/_app.js
@@ -1,10 +1,12 @@
 import GlobalHeader from "../components/GlobalHeader";
+import HeadMeta from "../components/HeadMeta";
 import PageWrapper from "../components/PageWrapper";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <PageWrapper>
+      <HeadMeta />
       <GlobalHeader />
       <Component {...pageProps} />
     </PageWrapper>
