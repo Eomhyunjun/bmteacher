4c6769ec77c119f583158ada532aba017c93b1eb
Eomhyunjun <zzp1318@gmail.com>
2022-12-20 20:43:04 +0900

    SEO: title, description 추가

--- bmfront/components/HeadMeta.js
+++ bmfront/components/HeadMeta.js
@@ -1,11 +1,16 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 function HeadMeta(props) {
-    return (
-        <Head>
-            <meta name="naver-site-verification" content="e5b01ccf23389c9c3316a778fe94b575e0a8f699" />
-        </Head>
-    );
+  return (
+    <Head>
+      <title>반면교사</title>
+      <meta
+        name="naver-site-verification"
+        content="e5b01ccf23389c9c3316a778fe94b575e0a8f699"
+      />
+       <meta name="description" content="위대한 기업의 성공요인은 비슷하고 어쩌면 똑같지만, 실패한 기업의 이유는 모두 다릅니다. 우리는 실패한 사례를 통하여 성장하고자 합니다." />
+    </Head>
+  );
 }
 
 export default HeadMeta;
--- bmfront/pages/index.js
+++ bmfront/pages/index.js
@@ -10,12 +10,13 @@ export default function Home() {
           name="naver-site-verification"
           content="e5b01ccf23389c9c3316a778fe94b575e0a8f699"
         />
-        <meta title="반면교사" />
       </Head>
       <MainText>반면교사[反面敎師]</MainText>
       <SubText>
-        반면교사는 다른 사람의 잘못된 일과 실패를 거울삼아 나의 가르침으로
-        삼는다는 뜻입니다.
+      위대한 기업의 성공요인은 비슷하고 어쩌면 똑같지만,
+      <p>실패한 기업의 이유는 모두 다릅니다.</p>
+      
+      <p>우리는 실패한 사례를 통하여 성장하고자 합니다</p>
       </SubText>
       <MovePage_a
         as="a"
