2017da9f1c45ce7a32bce5bdc0fb694338adef15
Eomhyunjun <zzp1318@gmail.com>
2022-12-28 15:37:24 +0900

    데이터 수정

--- bmfront/components/HeadMeta.js
+++ bmfront/components/HeadMeta.js
@@ -3,12 +3,12 @@ import Head from "next/head";
 function HeadMeta(props) {
   return (
     <Head>
-      <title>반면교사</title>
+      <title>스타트업 반면교사</title>
       <meta
         name="naver-site-verification"
         content="e5b01ccf23389c9c3316a778fe94b575e0a8f699"
       />
-       <meta name="description" content="위대한 기업의 성공요인은 비슷하고 어쩌면 똑같지만, 실패한 기업의 이유는 모두 다릅니다. 우리는 실패한 사례를 통하여 성장하고자 합니다. / 창업 실패 사례" />
+       <meta name="description" content="위대한 기업의 성공요인은 비슷하고 어쩌면 똑같지만, 실패한 기업의 이유는 모두 다릅니다. 우리는 실패한 사례를 통하여 성장하고자 합니다." />
     </Head>
   );
 }
--- bmfront/global/notionUrl.js
+++ bmfront/global/notionUrl.js
@@ -1,1 +1,1 @@
-export const notionUrl = 'https://heom.notion.site/d634637cc9e04b98a5cae68865c84e9a';
+export const notionUrl = 'https://nforu.notion.site/d634637cc9e04b98a5cae68865c84e9a';
